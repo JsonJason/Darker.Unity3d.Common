8dab6881e608433ae9154f5a0da54c882fc7764a
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-17 05:18:02 +0000

    Add project files.

---
+++ Darker.Unity3d.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darker.Unity3d.Common
+{
+    public class Class1
+    {
+    }
+}
