8ddf296779451e31a1300919aa3dc4f54caee3de
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-17 05:59:53 +0000

    Runs Resharpers Code Cleaner

--- Darker.Unity3d.Common/Services/Time/ITimeService.cs
+++ Darker.Unity3d.Common/Services/Time/ITimeService.cs
@@ -1,24 +1,25 @@
 ﻿namespace Darker.Unity3d
 {
     /// <summary>
-    /// Provides data about the current timescale and time between updates
+    ///     Provides data about the current timescale and time between updates
     /// </summary>
     public interface ITimeService
     {
         /// <summary>
-        /// Time since last update
+        ///     Time since last update
         /// </summary>
         float Delta { get; }
 
         /// <summary>
-        /// Current Scale of Time. <example>0 - Stopped,1 - Normal,2 - DoubleTime etc</example> 
+        ///     Current Scale of Time.
+        ///     <example>0 - Stopped,1 - Normal,2 - DoubleTime etc</example>
         /// </summary>
         float TimeScale { get; set; }
 
         /// <summary>
-        /// Stop Time
+        ///     Stop Time
         /// </summary>
-        /// <returns>The <see cref="TimeScale"/> before time was stopped</returns>
+        /// <returns>The <see cref="TimeScale" /> before time was stopped</returns>
         float StopTime();
     }
 }
