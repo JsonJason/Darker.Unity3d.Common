a9c7eee18cd57f008788fadd5ac30952206481d5
Jason Storey <jasonstoreyireland@gmail.com>
2017-11-17 05:24:53 +0000

    Adds input service

---
+++ Darker.Unity3d.Common/Services/Input/InputService.cs
@@ -0,0 +1,12 @@
+﻿namespace Darker.Unity3d
+{
+    public interface InputService
+    {
+        bool GetDown(string action);
+        bool Get(string action);
+        bool GetUp(string action);
+
+        float GetAxis(string name);
+        float GetAxisRaw(string name);
+    }
+}
---
+++ Darker.Unity3d.Common/Services/Input/UnityInputService.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Darker.Unity3d
+{
+    public class UnityInputService : InputService
+    {
+        public bool GetDown(string action) => Input.GetButtonDown(action);
+        public bool Get(string action) => Input.GetButton(action);
+        public bool GetUp(string action) => Input.GetButtonUp(action);
+        public float GetAxis(string name) => Input.GetAxis(name);
+        public float GetAxisRaw(string name) => Input.GetAxisRaw(name);
+    }
+}
