fab950e1ae53da82e04dba5131b57b3f3a472627
Jason Storey <jasonstoreyireland@gmail.com>
2018-01-22 12:54:01 +0000

    adds coroutine runner

---
+++ Darker.Unity3d.Common/Mechanics/Coroutines/CoroutineRunner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Darker.Unity3d
+{
+    /// <summary>
+    ///     Runs all coroutines using the provided MonoBehaviour as the parent.
+    /// </summary>
+    public class CoroutineRunner : ICoroutineRunner
+    {
+        private readonly MonoBehaviour _owner;
+
+        /// <summary>
+        ///     Constructs a coroutine runner that uses a monobehaviour, standard unity implementation
+        /// </summary>
+        /// <param name="owner">MonoBehaviour to host the coroutines</param>
+        public CoroutineRunner(MonoBehaviour owner)
+        {
+            _owner = owner;
+        }
+
+        /// <inheritdoc />
+        public void Start(IEnumerator enumerator)
+        {
+            _owner.StartCoroutine(enumerator);
+        }
+
+        /// <inheritdoc />
+        public void Stop(IEnumerator enumerator)
+        {
+            _owner.StartCoroutine(enumerator);
+        }
+
+        /// <inheritdoc />
+        public void StopAll()
+        {
+            _owner.StopAllCoroutines();
+        }
+    }
+}
---
+++ Darker.Unity3d.Common/Mechanics/Coroutines/ICoroutineRunner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Darker.Unity3d
+{
+    /// <summary>
+    ///     Starts and Stops Coroutines, Enumerators designed to be run across many frames
+    /// </summary>
+    public interface ICoroutineRunner
+    {
+        /// <summary>
+        ///     Starts running the enumerator
+        /// </summary>
+        /// <param name="enumerator">enumerator</param>
+        void Start(IEnumerator enumerator);
+
+        /// <summary>
+        ///     Stops running the enumerator
+        /// </summary>
+        /// <param name="enumerator">enumerator</param>
+        void Stop(IEnumerator enumerator);
+
+        /// <summary>
+        ///     Stops all running enumerators
+        /// </summary>
+        void StopAll();
+    }
+}
